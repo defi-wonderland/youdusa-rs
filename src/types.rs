ca4185accd8fa7c3c05f0f2a180e16a195fbf0e9
drgorillamd <83670532+drgorillamd@users.noreply.github.com>
2025-02-06 17:17:59 +0100

    fix: msg value and u256

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,9 @@
+use primitive_types::U256;
+
 #[derive(Debug)]
 pub struct CheatsData {
     pub block_to_roll: i32,
     pub timestamp_to_warp_to: i32,
     pub caller_to_prank: String,
-    pub value: i32,
+    pub value: U256,
 }
